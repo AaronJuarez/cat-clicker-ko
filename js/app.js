9b32001a06c654b824d6649b343ed20e3307935c
Aaron Juarez <aarjuamtz@gmail.com>
2016-06-18 16:24:29 -0500

    Fix: Separate the Model

--- js/app.js
+++ js/app.js
@@ -1,28 +1,85 @@
-var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            nicknames: ['Tabs', 'furry', 'tabbys']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nicknames: ['Tigger']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nicknames: ['Casper']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nicknames: ['Shobby']
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nicknames: ['Zzzzz']
+        }
+    ]
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.level = ko.computed(function() {
-    	var strLevel = 'newborn';
-    	if (this.clickCount() >= 10 && this.clickCount() < 100) {
-    		strLevel = 'infant';
-    	} else {
-    		if (this.clickCount() >= 100 && this.clickCount() < 200) {
-    			strLevel = 'teen';
-    		} else {
-    			if (this.clickCount() >= 200) {
-    				strLevel = 'adult';
-    			}
-    		}
-    	}
-    	return strLevel;
+        var strLevel = '';
+        var clicks = this.clickCount();
+
+        if (clicks < 10) {
+            strLevel = 'newborn';
+        } else if (clicks < 100) {
+            strLevel = 'infant';
+        } else if (clicks < 200) {
+            strLevel = 'child';
+        } else if (clicks < 300) {
+            strLevel = 'teen'
+        } else if (clicks < 400) {
+            strLevel = 'adult';
+        } else {
+            strLevel = 'ninja';
+        };
+        return strLevel;
     }, this);
-}
+
+};
+
+var ViewModel = function() {
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
+
+    this.incrementCounter = function() {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+};
 
 ko.applyBindings(new ViewModel());
